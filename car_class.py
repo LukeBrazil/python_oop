b74487e72b64113c9e2c20da08c15850bb4439a7
Luke Brazil <Luke.brazil13@icloud.com>
2020-08-03 13:52:11 -0500

    python class page on a spaceship.

---
+++ car_class.py
@@ -0,0 +1,18 @@
+class Spaceship:
+    shape: 'oval'
+    hyperdrive: 'Gravity Propelled'
+    galaxy: 'Juilian 647'
+
+    def __init__(self, name, make, model, year):
+        self.name = name
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def __str__(self):
+        return "This spaceship is called the %s. An %s %s, created in the year %d." % (self.name, self.make, self.model, self.year)
+
+
+Millenium_Falcon = Spaceship("Millenium Falcon", "CEC", "YT-Series", 1300)
+
+print(Millenium_Falcon)
